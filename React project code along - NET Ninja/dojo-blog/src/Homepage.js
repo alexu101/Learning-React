4c713770deeb485f45d9d306f4a2e0e6f36e5277
Tanase Alex <alextanase60@yahoo.com>
2022-02-08 20:32:45 +0200

    outputting list and css for that lists(blogs)

--- React project code along - NET Ninja/dojo-blog/src/Homepage.js
+++ React project code along - NET Ninja/dojo-blog/src/Homepage.js
@@ -2,20 +2,35 @@ import { useState } from 'react';
 
 const Homepage = () => {
 
-
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = function () {
-        setName('luigi');
-        setAge(20);
-    }
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'My new website',
+            body: 'lorem ipsum....',
+            author: 'mario',
+            id: 1
+        },
+        {
+            title: 'Welcome to my party!',
+            body: 'lorem ipsum....',
+            author: 'yoshi',
+            id: 2
+        },
+        {
+            title: 'Web dev top tips',
+            body: 'lorem ipsum....',
+            author: 'mario',
+            id: 3
+        }
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <p>{name} is {age}</p>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author} !</p>
+                </div>
+            ))}
         </div>
     );
 }

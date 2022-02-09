410115e8ababd75fb829ba4faa951fced06f663a
Tanase Alex <alextanase60@yahoo.com>
2022-02-09 16:40:52 +0200

    passing function to props

--- React project code along - NET Ninja/dojo-blog/src/Homepage.js
+++ React project code along - NET Ninja/dojo-blog/src/Homepage.js
@@ -24,9 +24,14 @@ const Homepage = () => {
         }
     ]);
 
+    const handleDelete = function (id) {
+        const newBlogs = blogs.filter(blog => id != blog.id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={'All Blogs'} />
+            <BlogList blogs={blogs} title={'All Blogs'} handleDelete={handleDelete} />
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={`Mario's blogs`} />
         </div>
     );

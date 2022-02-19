ca3c637b503e0b1eca710cc472a6f70258250672
Tanase Alex <alextanase60@yahoo.com>
2022-02-19 10:30:52 +0200

    i learned again about useFetch reusabilty

--- React project code along - NET Ninja/dojo-blog/src/BlogDetails.js
+++ React project code along - NET Ninja/dojo-blog/src/BlogDetails.js
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
+
 const BlogDetails
     = () => {
-        const { id } = useParams();
-        const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
+        const { id } = useParams();
+        const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id)
 
         return (
             <div className="blog-details">
@@ -14,7 +15,7 @@ const BlogDetails
                 {blog && (
                     <article>
                         <h2>{blog.title}</h2>
-                        <p>Written by {blog.author} .</p>
+                        <p>Written by {blog.author}</p>
                         <div>{blog.body}</div>
                     </article>
                 )}
